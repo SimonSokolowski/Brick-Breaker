07007d4bd6b8e70f1816ab7c98cfbf5d04f8ea14
simon <simon.sokolowski@outlook.com>
2021-01-03 18:29:43 +0000

    fixed bugs

--- Engine/Vec2.h
+++ Engine/Vec2.h
@@ -7,8 +7,12 @@ public:
 	Vec2(float x_in, float y_in);
 	Vec2 operator+(const Vec2& rhs) const; // lhs + rhs
 	Vec2& operator+=(const Vec2& rhs);
-	Vec2 operator*(float rhs) const; // lhs + rhs
+	Vec2 operator*(float rhs) const; // lhs * rhs
 	Vec2& operator*=(float rhs);
+	Vec2 operator-(const Vec2& rhs) const; // lhs - rhs
+	Vec2& operator-=(const Vec2& rhs);
+	float GetLength() const;
+	float GetLengthSq() const;
 public:
 	float x;
 	float y;
